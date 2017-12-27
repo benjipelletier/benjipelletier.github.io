a97c61fe6674c889bb3183c80ac80c142f390363
benjipelletier <pelletierbenji@gmail.com>
2017-12-27 12:50:44 +0800

    fixed icons/images

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,7 @@
 
 var mainApp = angular.module('mainApp',[]);
 
-mainApp.controller('MainController', ['$scope', function($scope) {
+mainApp.controller('MainController', ['$scope', '$sce', function($scope, $sce) {
   $scope.sideProjects = [
     {title: "The Room Soundboard", 
      desc: 'Your favourite sound clips from the cult classic "The Room"', 
@@ -14,7 +14,7 @@ mainApp.controller('MainController', ['$scope', function($scope) {
      links: ["https://itunes.apple.com/us/app/perfection-perfect-pitch-ear-training/id980947147?mt=8", "https://play.google.com/store/apps/details?id=me.benjipelletier.perfectionfree&hl=en"]},
     
     {title: "Searchonym", 
-     desc: "Searchonym finds matches on a webpage for the synonyms of a given word.", img: "images/shooting.png",
+     desc: "Searchonym finds matches on a webpage for the synonyms of a given word.", img: "images/searchonym.png",
      links: ["https://chrome.google.com/webstore/detail/searchonym/dcakhmpgjgoolamccholpcmjdbjembfg?hl=en"]},
     
     {title: "Mass Shooting Statistics", 
@@ -24,13 +24,13 @@ mainApp.controller('MainController', ['$scope', function($scope) {
     
     {title: "Linear Regression Calculator", 
      desc: "more info", 
-     img: "images/shooting.png",
+     img: "images/regression.png",
      links: ["/regression"]}
   ];
 
 $scope.workExp = [
-  {company: "Genesys", position: "Font End Developer", img: "images/genesys.png",
-  desc: "klmao oikejhkdf sdjf skdfn sd "}
+  {company: "Genesys", position: "Font End Developer", link: "http://www.genesys.com/", img: "images/genesys.png",
+  desc: "klmao oikejhkdf sdjf skdfn sd ", api_src: $sce.trustAsResourceUrl("//www.google.com/maps/embed/v1/place?q=Genesys,Markham&zoom=9&key=AIzaSyAmMXQiUtgnHPVW_M2h_zgbDYBAP2ySgpA")}
 ];
     $scope.currTab = 0;
     $scope.changeTab = function(tab){
