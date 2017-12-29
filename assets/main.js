18d48ae114f75c6d472ad7879296e368f473de71
benjipelletier <pelletierbenji@gmail.com>
2017-12-29 13:42:01 +0800

    Final update before host

--- assets/main.js
+++ assets/main.js
@@ -4,12 +4,12 @@ var mainApp = angular.module('mainApp',[]);
 mainApp.controller('MainController', ['$scope', '$sce', function($scope, $sce) {
   $scope.sideProjects = [
     {title: "The Room Soundboard", 
-     desc: 'Your favourite sound clips from the cult classic "The Room"', 
+     desc: 'Soundboard app to supplement the best worst move ever: "The Room".', 
      img: "images/theroom.png", 
      links: ["https://itunes.apple.com/ca/app/the-room-movie-soundboard/id878083729?mt=8", "https://play.google.com/store/apps/details?id=com.breadedturtle.TheRoomSoundboard"]},
     
     {title: "Perfection", 
-     desc: "Perfection allows people with perfect pitch to practice and improve their ability to identify pitch.", 
+     desc: "Perfection allows people with perfect pitch to improve their ability to identify pitch.", 
      img: "images/perfection.png",
      links: ["https://itunes.apple.com/us/app/perfection-perfect-pitch-ear-training/id980947147?mt=8", "https://play.google.com/store/apps/details?id=me.benjipelletier.perfectionfree&hl=en"]},
     
@@ -17,20 +17,20 @@ mainApp.controller('MainController', ['$scope', '$sce', function($scope, $sce) {
      desc: "Searchonym finds matches on a webpage for the synonyms of a given word.", img: "images/searchonym.png",
      links: ["https://chrome.google.com/webstore/detail/searchonym/dcakhmpgjgoolamccholpcmjdbjembfg?hl=en"]},
     
-    {title: "Mass Shooting Statistics", 
-     desc: "more info", 
+    {title: "MS Statistics", 
+     desc: "Mass Shootings statistics for the United States in 2015.", 
      img: "images/shooting.png",
-     links: ["/massshootings"]},
+     links: ["massshootings/index.html"]},
     
-    {title: "Linear Regression Calculator", 
-     desc: "more info", 
+    {title: "Regression", 
+     desc: "Online simple (x, y) linear regression calculator using basic machine learning and calculus.", 
      img: "images/regression.png",
-     links: ["/regression"]}
+     links: ["regression/index.html"]}
   ];
 
 $scope.workExp = [
   {company: "Genesys", position: "Font End Developer", link: "http://www.genesys.com/", img: "images/genesys.png",
-  desc: "klmao oikejhkdf sdjf skdfn sd ", api_src: $sce.trustAsResourceUrl("//www.google.com/maps/embed/v1/place?q=Genesys,Markham&zoom=9&key=AIzaSyAmMXQiUtgnHPVW_M2h_zgbDYBAP2ySgpA")}
+  desc: "Developed numerous features and fixes for Genesys's GWT-based call centre management system including a dynamically adjustable schedule for users' data. Gained experience in unit and automated Java testing for various features on GWT applications.", api_src: $sce.trustAsResourceUrl("//www.google.com/maps/embed/v1/place?q=Genesys,Markham&zoom=9&key=AIzaSyAmMXQiUtgnHPVW_M2h_zgbDYBAP2ySgpA")}
 ];
     $scope.currTab = 0;
     $scope.changeTab = function(tab){
